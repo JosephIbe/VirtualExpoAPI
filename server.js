a714e3553501ee47d1adf154eb56461dd909af1c
JosephIbe <joseph.ibeawuchi@gmail.com>
2020-08-27 13:29:45 +0100

    Add Cors again

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const authenticateRoute = require('./routes/users.route');
 const stallsRoute = require('./routes/stalls.route');
 
 const app = express();
+
 app.use(cors());
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
